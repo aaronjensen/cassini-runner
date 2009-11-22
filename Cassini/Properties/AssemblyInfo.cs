aabc0626d925e1dfe885542ee38e8380674d4536
Aaron Jensen <aaronjensen@gmail.com>
2009-11-22 15:50:10 -0800

    Initial commit

---
+++ Cassini/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿/*=======================================================================
+  Copyright (C) Microsoft Corporation.  All rights reserved.
+ 
+  THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+  KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+  PARTICULAR PURPOSE.
+=======================================================================*/
+
+
+using System.Reflection;
+
+//
+// Assembly attributes for Cassini.dll
+//
+
+[assembly: AssemblyTitle("Cassini")]
+[assembly: AssemblyDescription("Cassini Web Server Sample")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("Cassini")]
+[assembly: AssemblyCopyright("Copyright © Microsoft Corporation 2001-2002. All Rights Reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.40305.0")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("Cassini.snk")]
+[assembly: AssemblyKeyName("")]
---
+++ CassiniRunner/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading;
+
+namespace Cassini
+{
+  class Program
+  {
+    static Server _server;
+    static string _appPath;
+    static int _portNumber;
+    static string _virtRoot;
+
+    static void Main(string[] args)
+    {
+      ParseArgs(args);
+
+      var mutex = new Mutex(false, "mycassini" + _portNumber);
+      var mutex2 = new Mutex(false, "mycassinirunning" + _portNumber);
+
+      if (_appPath == "stop")
+      {
+        mutex.WaitOne();
+        mutex2.WaitOne();
+
+        mutex2.ReleaseMutex();
+        mutex.ReleaseMutex();
+        return;
+      }
+
+      _server = new Cassini.Server(_portNumber, _virtRoot, _appPath);
+
+      try
+      {
+        mutex2.WaitOne();
+        _server.Start();
+      }
+      catch (Exception er)
+      {
+        Console.WriteLine(er);
+      }
+
+      while (mutex.WaitOne(0))
+      {
+        mutex.ReleaseMutex();
+        Thread.Sleep(50);
+      }
+
+      mutex2.ReleaseMutex();
+    }
+
+    private static void ParseArgs(String[] args)
+    {
+      string portString = null;
+      try
+      {
+        if (args.Length >= 1)
+          _appPath = args[0];
+
+        if (args.Length >= 2)
+          portString = args[1];
+
+        if (args.Length >= 3)
+          _virtRoot = args[2];
+      }
+      catch
+      {
+      }
+
+      if (portString == null)
+        portString = "80";
+
+      _portNumber = Int32.Parse(portString);
+      if (_virtRoot == null)
+        _virtRoot = "/";
+
+      if (_appPath == null)
+        _appPath = String.Empty;
+    }
+
+  }
+}
